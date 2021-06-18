4f424ff15a69496520135745a388e70ce70d8556
Maikon Takeshi Tsujiguchi <maikon_numb@hotmail.com>
2021-06-18 20:48:51 -0300

    Adicionado configuração para injeção de dependência

---
+++ Infrastructure.DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+        public static void GetModules(IServiceCollection services)
+        {
+
+        }
+    }
+}
