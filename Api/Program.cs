debb3645b1b02ef76892411ede1233552acc50f0
Maikon Takeshi Tsujiguchi <maikon_numb@hotmail.com>
2021-06-19 00:09:07 -0300

    Adicionado configurações do appmetrics

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,5 +1,12 @@
+using App.Metrics;
+using App.Metrics.AspNetCore;
+using Domain.Configurations;
+using Infrastructure.CrossCutting.InfluxDb;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
 
 namespace Api
 {
@@ -10,11 +17,33 @@ namespace Api
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var configuration = GetConfiguration();
+            var influxDbConfiguration = configuration.GetSection("InfluxDbConfiguration").Get<InfluxDbConfiguration>();
+
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.ConfigureMetricsWithDefaults(builder =>
+
+                    {
+                        builder.Report.ToInfluxDb(options =>
+                        {
+                            InfluxDbConnection.SetConfigurations(options, influxDbConfiguration);
+                        });
+                    });
+                    webBuilder.UseMetrics();
                     webBuilder.UseStartup<Startup>();
                 });
+        }
+
+        private static IConfiguration GetConfiguration()
+            => new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .AddJsonFile($"appsetting.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true, reloadOnChange: true)
+                    .AddEnvironmentVariables()
+                    .Build();
     }
 }
